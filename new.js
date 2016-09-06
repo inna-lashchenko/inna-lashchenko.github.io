e0dee83dc9509c34bb6af45c23c668b9a87f4664
innkognito <inn4633@gmail.com>
2016-09-06 10:36:21 +0300

    Update new.js

--- new.js
+++ new.js
@@ -1,30 +1,47 @@
 
 window.addEventListener("load", start, false);
 
-function Card(suit, val, name, sign)
+function start()
+{
+    newGame();
+}
+
+function newGame()
+{
+    var deck1 = new Deck();
+    deck1.createDeck();
+    deck1.shuffle();
+    deck1.deal();
+    document.getElementById("hit").addEventListener("click", function(){deck1.hit();},false);
+    document.getElementById("stand").addEventListener("click", function(){deck1.stand();},false);
+    document.getElementById("replay").addEventListener("click", function(){deck1.deal();}, false);
+}
+
+
+function Card(suit, val, sign)
 {
     this.suit = suit;
     this.val = val;
-    this.name = name;
     this.sign = sign;
+}
 
-    this.showCard =function showCard()
+Card.prototype.showCard =function showCard()
+{
+    var html="", suit_text="";
+
+    switch(this.suit)
     {
-        var html="";
-        switch(this.suit)
-        {
-            case "hearts": suit_text = "&hearts;";
-                break;
-            case "diamonds": suit_text = "&diams;";
-                break;
-            case "spades": suit_text = "&spades;";
-                break;
-            case "clubs": suit_text = "&clubs;";
-                break;
-        }
-        html="<div class='card " + this.suit + "'><div class='card-value'>" + this.sign + "</div><div class='suit'>" + suit_text + "</div><div class='main-number'>"+this.sign +"</div><div class='invert card-value'>"+this.sign+"</div><div class='invert suit'>"+suit_text+"</div></div>";
-        return html;
+        case "hearts": suit_text = "&hearts;";
+            break;
+        case "diamonds": suit_text = "&diams;";
+            break;
+        case "spades": suit_text = "&spades;";
+            break;
+        case "clubs": suit_text = "&clubs;";
+            break;
     }
+    html="<div class='card " + this.suit + "'><div class='card-value'>" + this.sign + "</div><div class='suit'>" + suit_text + "</div><div class='main-number'>"+this.sign +"</div><div class='invert card-value'>"+this.sign+"</div><div class='invert suit'>"+suit_text+"</div></div>";
+        return html;
 }
 
 
@@ -51,7 +68,7 @@ function Deck()
     this.createDeck = function createDeck()
     {
         var numCards=0;
-        var suit, sign, name;
+        var suit, sign;
         for(var k=1; k<=4; k++)
         {
             switch(k)
@@ -68,41 +85,20 @@ function Deck()
 
             for(var i=1; i<=13; i++)
             {
-                sign = i;
                 switch(i)
                 {
-                    case 1: name= "Ace";
-                        sign = "A";
+                    case 1: sign = "A";
                         break;
-                    case 2: name= "two";
+                    case 11: sign = "J";
                         break;
-                    case 3: name= "three";
+                    case 12: sign = "Q";
                         break;
-                    case 4: name= "four";
+                    case 13: sign = "K";
                         break;
-                    case 5: name= "five";
-                        break;
-                    case 6: name= "six";
-                        break;
-                    case 7: name= "seven";
-                        break;
-                    case 8: name= "eight";
-                        break;
-                    case 9: name= "nine";
-                        break;
-                    case 10: name= "ten";
-                        break;
-                    case 11: name= "jack";
-                        sign = "J";
-                        break;
-                    case 12: name= "queen";
-                        sign = "Q";
-                        break;
-                    case 13: name= "king";
-                        sign = "K";
+                    default: sign = i;
                         break;
                 }
-                this.deck[numCards] = new Card(suit, i, name, sign);
+                this.deck[numCards] = new Card(suit, i, sign);
                 numCards++;
             }
         }
@@ -128,17 +124,23 @@ function Deck()
 
     this.getValue = function getValue(hand)
     {
-        var val = 0;
-        var tempArr = hand;
-        tempArr.sort(function(a,b) { return parseFloat(a.val) - parseFloat(b.val) } );
-        for(var i=tempArr.length-1; i>=0; i--)
-        {
-            var temp = tempArr[i];
-            if(temp.val === 1 && val <=10)temp.val = 11;
-            else if(temp.val >=10) temp.val = 10;
-            val += temp.val;
+        console.log(hand);
+        var result=0, aces=0, temp=0;
+        for(var i=0; i<hand.length; i++){
+            if(hand[i].val>=10){temp=10;}
+            else if(hand[i].val==1){temp=11; aces++;}
+            else temp=hand[i].val;
+            result+=temp;
         }
-        return val;
+        if(result>21&&aces>0){
+            do{
+                result=result-10;
+                aces--;
+            }
+            while(result>21&&aces>0)
+        }
+
+        return result;
     };
 
     this.emptyDeck = function emptyDeck()
@@ -149,46 +151,51 @@ function Deck()
 
     this.deal = function deal()
     {
-        status.innerHTML="";
-        this.money--;
-        money.innerHTML= "YOUR MONEY: " + this.money+"$";
-        dealerHand.innerHTML="<h2>Dealer's Hand:</h2>";
-        userHand.innerHTML="<h2>Your Hand:</h2>";
-        this.userTotal=0;
-        this.dealerTotal=0;
-        this.userBust=false;
-        this.dealerBust=false;
-        hit.setAttribute("style", "");
-        stand.setAttribute("style", "");
-        dealerScore.setAttribute("style", "");
-        this.currentUser = [];
-        this.currentDealer = [];
-        result.setAttribute("style","visibility:visible");
-
-
-        for(i=0; i<2; i++)
-        {
-            if(this.emptyDeck())this.newDeck();
-            this.currentUser.push(this.deck.pop());
-            userHand.innerHTML+=this.currentUser[i].showCard();
+        if(this.money<=0){
+            result.innerHTML="OUT OF MONEY";
+            hit.setAttribute("style", "visibility:hidden;");
+            stand.setAttribute("style", "visibility:hidden;");
+            replay.setAttribute("style", "visibility:hidden;");
         }
-        this.userTotal = this.getValue(this.currentUser);
-        userScore.innerHTML=this.userTotal;
+        else {
+            this.money--;
+            status.innerHTML = "";
+            moneyDiv.innerHTML = "YOUR MONEY: " + this.money + "$";
+            dealerHand.innerHTML = "<h2>Dealer's Hand:</h2>";
+            userHand.innerHTML = "<h2>Your Hand:</h2>";
+            this.userTotal = 0;
+            this.dealerTotal = 0;
+            this.userBust = false;
+            this.dealerBust = false;
+            hit.setAttribute("style", "");
+            stand.setAttribute("style", "");
+            dealerScore.setAttribute("style", "");
+            this.currentUser = [];
+            this.currentDealer = [];
+            result.setAttribute("style", "visibility:visible");
 
-        for(i=0; i<2; i++)
-        {
-            if(this.emptyDeck())this.newDeck();
-            this.currentDealer.push(this.deck.pop());
-            dealerHand.innerHTML+=this.currentDealer[i].showCard();
+
+            for (var i = 0; i < 2; i++) {
+                if (this.emptyDeck())this.newDeck();
+                this.currentUser.push(this.deck.pop());
+                userHand.innerHTML += this.currentUser[i].showCard();
+            }
+            this.userTotal = this.getValue(this.currentUser);
+            userScore.innerHTML = this.userTotal;
+
+            for (var i = 0; i < 2; i++) {
+                if (this.emptyDeck())this.newDeck();
+                this.currentDealer.push(this.deck.pop());
+                dealerHand.innerHTML += this.currentDealer[i].showCard();
+            }
+            this.dealerTotal = this.getValue(this.currentDealer);
+            dealerScore.innerHTML = this.dealerTotal;
+
+
+            var blackjack = true;
+            if (this.userTotal === 21 && this.dealerTotal < 21) this.gameOver(blackjack);
+            else if (this.dealerTotal === 21) this.gameOver();
         }
-        this.dealerTotal = this.getValue(this.currentDealer);
-        dealerScore.innerHTML=this.dealerTotal;
-
-        var firstCard = dealerHand.getElementsByClassName("card")[0];
-        firstCard.setAttribute("id", "hidden-card");
-        var blackjack =true;
-        if(this.userTotal === 21 && this.dealerTotal < 21) this.gameOver(blackjack);
-        else if(this.dealerTotal === 21) this.gameOver();
     };
 
     this.hit = function hit()
@@ -227,7 +234,7 @@ function Deck()
 
     this.gameOver = function gameOver(blackjack)
     {
-        document.getElementById("hidden-card").setAttribute("id","");
+
 
         hit.setAttribute("style", "visibility:hidden;");
         stand.setAttribute("style", "visibility:hidden;");
@@ -260,7 +267,6 @@ function Deck()
         else {result.setAttribute("style","visibility:hidden");
             status.innerHTML="YOU LOSE!";}
 
-        money.innerHTML="YOUR MONEY: "+this.money+"$";
 
     }
 
@@ -274,20 +280,4 @@ function Deck()
 }
 
 
-function play()
-{
-    var deck1 = new Deck();
-    deck1.createDeck();
-    deck1.shuffle();
-    deck1.deal();
-    document.getElementById("hit").addEventListener("click", function(){deck1.hit();},false);
-    document.getElementById("stand").addEventListener("click", function(){deck1.stand();},false);
-    document.getElementById("replay").addEventListener("click", function(){deck1.deal();}, false);
-}
-
-
-function start()
-{
-    play();
-}
 
